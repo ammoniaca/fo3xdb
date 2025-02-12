f19b8a66c7fb173fd2e82310ebd2f7a050aa0d3e
Alessandro <alessandro.montaghi@gmail.com>
2025-02-12 18:38:41 +0100

    initial commit

---
+++ src/main/java/org/cnr/fo3xdb/Fo3xdbApplication.java
@@ -0,0 +1,13 @@
+package org.cnr.fo3xdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fo3xdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fo3xdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cnr/fo3xdb/Fo3xdbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cnr.fo3xdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fo3xdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
