d4f6a47e13800f1c2b3dd3353cf7fb38b1fdd0f8
Alessandro Montaghi <alessandro.montaghi@gmail.com>
2025-02-13 19:51:46 +0100

    Create class DTO FoxHourlyDataDTO

---
+++ src/main/java/org/cnr/fo3xdb/dto/FoxHourlyDataDTO.java
@@ -0,0 +1,34 @@
+package org.cnr.fo3xdb.dto;
+
+
+
+public class FoxHourlyDataDTO {
+
+    private Double rainTotal;
+    private Double rainIntensityMax;
+    private Double airTemperatureMin;
+    private Double airTemperatureMax;
+    private Double airTemperatureAvg;
+    private Double airHumidity;
+    private Double dewPoint;
+    private Double airPressureAvg;
+    private Double solarRadiationMin;
+    private Double solarRadiationAvg;
+    private Double solarRadiationMax;
+    private Double windSpeedAvg;
+    private Double windDirectionAvg;
+    private Double windDirectionSTD;
+    private Double windSpeedMax;
+    private Double windDirectionAtMaximumSpeed;
+    private Double evapotranspiration;
+    private Double solarRadiationCalculated;
+    private Double batteryVoltage;
+    private Double dataLoggerTemperature;
+    private Double airTemperatureMeasurementErrors;
+    private Double airHumidityMeasurementErrors;
+    private Double atmosphericPressureMeasurementErrors;
+    private Double solarRadiationMeasurementErrors;
+    private Double windMeasurementErrors;
+
+
+}
