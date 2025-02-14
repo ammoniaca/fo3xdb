45af333aa8a29f7221027b95c116e86b2e6a4ab7
Alessandro Montaghi <alessandro.montaghi@gmail.com>
2025-02-14 17:45:04 +0100

    Repository FoxHourlyRecordRepository

---
+++ src/main/java/org/cnr/fo3xdb/repository/FoxHourlyRecordRepository.java
@@ -0,0 +1,13 @@
+package org.cnr.fo3xdb.repository;
+
+import org.cnr.fo3xdb.entity.FoxHourlyRecordEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoxHourlyRecordRepository extends
+        JpaRepository<FoxHourlyRecordEntity, Long> {
+
+
+
+}
