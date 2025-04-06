8f72a1b85eb40017771a7ac8b7cdebf936952452
Alessandro <alessandro.montaghi@gmail.com>
2025-04-06 22:00:09 +0200

    changed swagger with description

--- src/main/java/org/cnr/fo3xdb/configuration/OpenAPIConfig.java
+++ src/main/java/org/cnr/fo3xdb/configuration/OpenAPIConfig.java
@@ -26,7 +26,19 @@ public class OpenAPIConfig {
                                 .url("https://choosealicense.com/licenses/mit/")
                         )
                         .description("""
-                        This API exposes endpoints to manage the FO3X database.
+                        This API exposes endpoints to manage the FO3X database. FO3X O3-FACE (Free-air O3 eXposure) is
+                        a multidisciplinary study to assess the effects of increasing tropospheric ozone and other
+                        stress factors on vegetation under open-air conditions. FO3X is an ecosystem-level manipulative 
+                        research facility that ensures long-term operability, fully replicated treatment plots 
+                        connected with state-of-the-art ecophysiology and genomics labs and is available for 
+                        undertaking new collaborative projects. FO3X consists of nine 5x5x2m blocks in which the 
+                        concentrations of tropospheric ozone and other stress factors (e.g., drought, nitrogen, 
+                        pathogens) can be controlled. The design is a split-plot experiment that provides the ability 
+                        to assess the effects of ozone and other stressors, alone and in combination, 
+                        on many plant attributes, including growth, root characteristics, gas exchanges, biogenic 
+                        volatile organic compounds, nutrients as well as antioxidants. At present, plants are potted 
+                        but can be planted in the ground if needed.
+                        
                         ### Authors
                         - **Elena Marra** - Institute of Research on Terrestrial Ecosystems (IRET) of the National Research Council (CNR), Italy
                         - **Alessandro Montaghi** - Institute of Research on Terrestrial Ecosystems (IRET) of the National Research Council (CNR), Italy
